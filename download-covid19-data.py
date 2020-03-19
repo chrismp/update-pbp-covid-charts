84a7391ec4328116e67fd56c5dd045c45c2ba525
Chris P <christopher.mark.persaud@gmail.com>
2020-03-19 19:39:40 -0400

    Scraper compares downloaded file to latest-downloaded file - Scraper downloads file from FLDOH, compares its file size to file size of latest downloaded file. If they're the same, delete the temporarily downloaded file. Else rename the temp file and put it in the output file

--- download-covid19-data.py
+++ download-covid19-data.py
@@ -1,21 +1,22 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[21]:
+# In[1]:
 
 
-import requests, json, csv, datetime, sys
+import requests, json, csv, datetime, filecmp, os, sys, glob
 from collections import OrderedDict
 
 
-# In[22]:
+# In[2]:
 
 
 url = "https://services1.arcgis.com/CY1LXxl9zlJeBuRZ/ArcGIS/rest/services/Detailed_COVID_19_Case_Data/FeatureServer/0/query?f=json&where=1%3D1&returnGeometry=false&outFields=*"
+response = requests.get(url).content
 j = requests.get(url).json()
 
 
-# In[36]:
+# In[3]:
 
 
 cases = [obj["attributes"] for obj in j["features"]]
@@ -25,14 +26,38 @@ for case in cases:
     data.append(OrderedDict(case))
 
 
-# In[37]:
+# In[4]:
 
 
-with open(sys.argv[1], "w", newline="") as outfile:
+tmpf = "temp.csv"
+with open(tmpf, "w", newline="") as outfile:
     writer = csv.writer(outfile)
     writer.writerow(list(cases[0].keys()))
     for row in cases:
         writer.writerow(list(row.values()))
+
+
+# In[5]:
+
+
+rawFiles = sorted([f for f in os.listdir("output/raw/")])
+latestFile = rawFiles[-1]
+
+
+# In[6]:
+
+
+tempAndLatestFileSameSize = os.path.getsize("output/raw/"+latestFile) == os.path.getsize(tmpf)
+if tempAndLatestFileSameSize:
+    print("Downloaded file is not an update. Removing '"+tmpf+"''")
+    os.remove(tmpf)
+    exit()
+
+
+# In[7]:
+
+
+os.rename(tmpf,sys.argv[1])
 
 
 # In[14]:
