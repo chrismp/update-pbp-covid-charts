3f8fce5268c019ed1fda2e5a09e538a514b2cef0
Chris P <christopher.mark.persaud@gmail.com>
2020-03-23 02:25:46 -0400

    Updating chart checks scraper return values

--- update-charts.r
+++ update-charts.r
@@ -10,87 +10,92 @@ datetime <- as.POSIXct(
   format = "%Y-%m-%d_%H%M%S"  
 )
 
-
-# Update South FL chart data
-sflChartTimestamp <- format(
-  x = datetime,
-  format = "%m/%d/%Y %H:%M"
-)
-
-countyCases <- read.csv(args[2])
-southFLCases <- read.csv(
-  file = args[3],
-  check.names = F,
-  stringsAsFactors = F
-)
-
-southFLCases <- rbind(
-  southFLCases,
-  c(
-    sflChartTimestamp,
-    countyCases[which(countyCases$County=="Broward"),]$Confirmed.cases,
-    countyCases[which(countyCases$County=="Dade"),]$Confirmed.cases,
-    countyCases[which(countyCases$County=="Palm Beach"),]$Confirmed.cases
+if(args[4]==0){
+  # Update South FL chart data
+  sflChartTimestamp <- format(
+    x = datetime,
+    format = "%m/%d/%Y %H:%M"
   )
-)
+  
+  countyCases <- read.csv(args[2])
+  southFLCases <- read.csv(
+    file = args[3],
+    check.names = F,
+    stringsAsFactors = F
+  )
+  
+  southFLCases <- rbind(
+    southFLCases,
+    c(
+      sflChartTimestamp,
+      countyCases[which(countyCases$County=="Broward"),]$Confirmed.cases,
+      countyCases[which(countyCases$County=="Dade"),]$Confirmed.cases,
+      countyCases[which(countyCases$County=="Palm Beach"),]$Confirmed.cases
+    )
+  )
+  
+  write.csv(
+    x = southFLCases,
+    file = args[3],
+    row.names = F
+  )
+  
+  
+  # Update annotation for each chart
+  updateDateFormat <- format(
+    x = datetime,
+    format = "%B %d, %Y"
+  )
+  
+  updateTimeFormat <- format(
+    x = datetime,
+    "%l:%M %P"
+  )
+  updateTimeFormat <- trimws(updateTimeFormat)
+  updateTimeFormat <- gsub(
+    pattern = 'm',
+    replacement = ".m.",
+    x = updateTimeFormat
+  )
+  
+  chartNote <- paste0("Figures reflect all known COVID-19 cases as of ",updateTimeFormat," on ",updateDateFormat,".")
+  
+  chartIDs <- list(
+    "county-cases-chart" = "Vdnj6",
+    "travel-related" = "ORvDZ",
+    "age-group" = "BSF3m",
+    "south-fl" = "aof13",
+    "cases-map" = "3OyJM"
+  )
+  
+  for (i in 1:length(chartIDs)) {
+    dw_edit_chart(
+      chart_id = chartIDs[[i]],
+      api_key = apikey,
+      annotate = chartNote
+    )
+    
+    dw_publish_chart(
+      chart_id = chartIDs[[i]],
+      api_key = apikey,
+      return_urls = TRUE
+    )  
+  }
+}
 
-write.csv(
-  x = southFLCases,
-  file = args[3],
-  row.names = F
-)
 
-
-# Update annotation for each chart
-updateDateFormat <- format(
-  x = datetime,
-  format = "%B %d, %Y"
-)
-
-updateTimeFormat <- format(
-  x = datetime,
-  "%l:%M %P"
-)
-updateTimeFormat <- trimws(updateTimeFormat)
-updateTimeFormat <- gsub(
-  pattern = 'm',
-  replacement = ".m.",
-  x = updateTimeFormat
-)
-
-chartNote <- paste0("Figures reflect all known COVID-19 cases as of ",updateTimeFormat," on ",updateDateFormat,".")
-
-chartIDs <- list(
-  "county-cases-chart" = "Vdnj6",
-  "travel-related" = "ORvDZ",
-  "age-group" = "BSF3m",
-  "south-fl" = "aof13",
-  "cases-map" = "3OyJM"
-)
-
-for (i in 1:length(chartIDs)) {
+# Update COVID19 testing map
+if(args[5]==0){
+  chartIDs[["tests-map"]] <- "4nU0g"
   dw_edit_chart(
-    chart_id = chartIDs[[i]],
+    chart_id = chartIDs[["tests-map"]],
     api_key = apikey,
-    annotate = chartNote
+    annotate = paste0("Figures reflect all known COVID-19 tests administered as of ",updateTimeFormat," on ",updateDateFormat,".")
   )
-
+  
   dw_publish_chart(
     chart_id = chartIDs[[i]],
     api_key = apikey,
     return_urls = TRUE
   )  
 }
-
-chartIDs[["tests-map"]] <- "4nU0g"
-dw_edit_chart(
-  chart_id = chartIDs[["tests-map"]],
-  api_key = apikey,
-  annotate = paste0("Figures reflect all known COVID-19 tests administered as of ",updateTimeFormat," on ",updateDateFormat,".")
-)
-
-dw_publish_chart(
-  chart_id = chartIDs[[i]],
-  api_key = apikey,
-  return_urls = TRUE
-) 
