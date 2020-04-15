7705d4f2b378bc80e5b6c8199cb548e6318dfd19
Chris P <christopher.mark.persaud@gmail.com>
2020-04-15 12:42:45 -0400

    Remove commented out code

--- make-datafiles-for-charts.r
+++ make-datafiles-for-charts.r
@@ -58,15 +58,6 @@ if(args[4]==0){
     no = "Rest of state"
   )
   
-  # correctUnixTimePositiveCases <- (positives$Case_/1000)
-  # positives$caseDate <- format(
-  #   x = as.POSIXct(
-  #     x = correctUnixTimePositiveCases,
-  #     origin = "1970-01-01",
-  #     tz = "EST"
-  #   ),
-  #   "%Y-%m-%d"
-  # )
   positives$caseDate <- as.Date(x = positives$Case_, format = "%m/%d/%Y")
   
   # statewide cumulative cases by date
